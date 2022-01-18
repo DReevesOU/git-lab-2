7e510498938c200f4e983c7baa913ed7e86ffad1
DReevesOU <dr300221@ohio.edu>
2022-01-18 11:47:58 -0500

    Adding lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Daniel Reeves
+ *        Date: 01/18/2022
+ * Description: Program for git-lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl; //added code
+    return 0;
+}// main
